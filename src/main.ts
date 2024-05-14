174ba1d91797094f7d61311a2666d692b2175656
Rishi Nath <rishi.nath@gmail.com>
2024-05-14 14:16:16 -0700

    three.js vite template

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './style.scss';
+
+// Toggle commented out code to run alternate demo.
+import Demo from './Demo';
+// import Demo from './Shader';
+
+window.addEventListener('DOMContentLoaded', () => {
+	new Demo();
+});
