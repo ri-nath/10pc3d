759bf8cedbeaaceaac437241009eed266e847908
Rishi Nath <rishi.nath@gmail.com>
2024-05-16 14:52:02 -0700

    minor refactor

--- src/camera/camera.js
+++ src/camera/camera.js
@@ -2,11 +2,10 @@ import * as THREE from 'three';
 
 let isCameraMoving = false
 let target = new THREE.Vector3(0, 0, 0)
-
-export function updateCamera(camera, controls, lerp_alpha=0.05, pos_cutoff=4) {
+export function updateCamera(camera, controls, lerp_alpha=0.01, pos_cutoff=5) {
     if (!(isCameraMoving && target)) return;
-    camera.position.lerp(target, 0.01);
-    controls.update();
+    camera.position.lerp(target, lerp_alpha);
+
     const pos_diff = camera.position.distanceTo(target)
     if (pos_diff < pos_cutoff) {
         isCameraMoving = false;
